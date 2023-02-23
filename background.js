a4e789a3444ed955cbf3c4d194a85cdce13164f7
Rajeshwar <kolimirajeshwarreddy@gmail.com>
2023-02-23 21:08:26 +0530

    Updated background.js

--- background.js
+++ background.js
@@ -1,170 +1,67 @@
-var NodeWebcam = require( "node-webcam" );
 
 
-//Default options
 
-var opts = {
+const nodeWebCam = require('node-webcam');
+const fs = require('fs');
+const app = require('express')();
+const express = require('express');
+const path = require('path');
 
-    //Picture related
+// npm install node-webcam fs path
 
-    width: 1280,
+app.use(express.static('images')) // images folder to be served
+// Now we can just say localhost:3000/image.jpg
 
-    height: 720,
+// specifying parameters for the pictures to be taken
+var options = {
+  width: 1280,
+  height: 720,
+  quality: 100,
+  delay: 1,
+  saveShots: true,
+  output: "jpeg",
+  device: false,
+  callbackReturn: "location"
+};
 
-    quality: 100,
+// create instance using the above options
+var webcam = nodeWebCam.create(options);
 
-    // Number of frames to capture
-    // More the frames, longer it takes to capture
-    // Use higher framerate for quality. Ex: 60
+// capture function that snaps <amount> images and saves them with the given name in a folder of the same name
+var captureShot = (amount, i, name) => {
+  // Make sure this returns a real url to an image.
+  return new Promise(resolve => {
+    var path = `./images/${name}`;
 
-    frames: 60,
-
-
-    //Delay in seconds to take shot
-    //if the platform supports miliseconds
-    //use a float (0.1)
-    //Currently only on windows
-
-    delay: 0,
-
-
-    //Save shots in memory
-
-    saveShots: true,
-
-
-    // [jpeg, png] support varies
-    // Webcam.OutputTypes
-
-    output: "jpeg",
-
-
-    //Which camera to use
-    //Use Webcam.list() for results
-    //false for default device
-
-    device: false,
-
-
-    // [location, buffer, base64]
-    // Webcam.CallbackReturnTypes
-
-    callbackReturn: "location",
-
-
-    //Logging
-
-    verbose: false
+    // create folder if and only if it does not exist
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path);
+    }
+    // capture the image
+    webcam.capture(`./images/${name}/${name}${i}.${options.output}`, (err, data) => {
+      if (!err) {
+        console.log('Image created')
+      }
+      console.log(err);
+      i++;
+      if (i <= amount) {
+        captureShot(amount, i, name);
+      }
+      resolve('/path/to/image.jpg')
+    });
+  })
 
 };
 
 
-//Creates webcam instance
-
-var Webcam = NodeWebcam.create( opts );
-
-
-//Will automatically append location output type
-
-Webcam.capture( "test_picture", function( err, data ) {} );
-
-
-//Also available for quick use
-
-NodeWebcam.capture( "test_picture", opts, function( err, data ) {
-
+app.get('/', (req, res) => {
+  captureShot(30, 1, 'robin')
+    .then((response) => {
+      // Whatever we resolve in captureShot, that's what response will contain
+      res.send('<img src="${response}"/>')
+    })
 });
 
-
-//Get list of cameras
-
-Webcam.list( function( list ) {
-
-    //Use another device
-
-    var anotherCam = NodeWebcam.create( { device: list[ 0 ] } );
-
+app.listen(3000, () => {
+  console.log("Listening at port 3000....");
 });
-
-//Return type with base 64 image
-
-var opts = {
-    callbackReturn: "base64"
-};
-
-NodeWebcam.capture( "test_picture", opts, function( err, data ) {
-
-    var image = "<img src='" + data + "'>";
-
-});
-
-
-
-
-
-
-
-// const nodeWebCam = require('node-webcam');
-// const fs = require('fs');
-// const app = require('express')();
-// const express = require('express');
-// const path = require('path');
-
-// // npm install node-webcam fs path
-
-// app.use(express.static('images')) // images folder to be served
-// // Now we can just say localhost:3000/image.jpg
-
-// // specifying parameters for the pictures to be taken
-// var options = {
-//   width: 1280,
-//   height: 720,
-//   quality: 100,
-//   delay: 1,
-//   saveShots: true,
-//   output: "jpeg",
-//   device: false,
-//   callbackReturn: "location"
-// };
-
-// // create instance using the above options
-// var webcam = nodeWebCam.create(options);
-
-// // capture function that snaps <amount> images and saves them with the given name in a folder of the same name
-// var captureShot = (amount, i, name) => {
-//   // Make sure this returns a real url to an image.
-//   return new Promise(resolve => {
-//     var path = `./images/${name}`;
-
-//     // create folder if and only if it does not exist
-//     if (!fs.existsSync(path)) {
-//       fs.mkdirSync(path);
-//     }
-//     // capture the image
-//     webcam.capture(`./images/${name}/${name}${i}.${options.output}`, (err, data) => {
-//       if (!err) {
-//         console.log('Image created')
-//       }
-//       console.log(err);
-//       i++;
-//       if (i <= amount) {
-//         captureShot(amount, i, name);
-//       }
-//       resolve('/path/to/image.jpg')
-//     });
-//   })
-
-// };
-
-
-// app.get('/', (req, res) => {
-//   captureShot(30, 1, 'robin')
-//     .then((response) => {
-//       // Whatever we resolve in captureShot, that's what response will contain
-//       res.send('<img src="${response}"/>')
-//     })
-// });
-
-// app.listen(3000, () => {
-//   console.log("Listening at port 3000....");
-// });
